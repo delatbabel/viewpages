5d2f1ac471c0b7b8c79085e03b844b500fa96f2a
Del <del@babel.com.au>
2016-02-01 14:04:31 +0800

    Amalgamated pages and templates models

--- database/seeds/examples/layouts/main.blade.php
+++ database/seeds/examples/layouts/main.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	<title>{{ $page_title or "OmniGate" }}</title>
+	<title>{{ $page_title or "Test Application" }}</title>
 	<meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
 	<!-- Bootstrap 3.3.4 -->
 	<link href="/bower_components/admin-lte/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
