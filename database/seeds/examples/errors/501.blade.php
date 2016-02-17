497f0d4740558634a5ff29fd1e65929b5d7cc308
Del <del@babel.com.au>
2016-02-17 10:33:05 +0800

    Add more custom error pages

---
+++ database/seeds/examples/errors/501.blade.php
@@ -0,0 +1,15 @@
+<html>
+  <style>
+    body {
+        background: #333333 }
+    section {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        margin-right: -50%;
+        transform: translate(-50%, -50%) }
+  </style>
+  <section>
+    <img src="http://www.babel.com.au/images/501.jpg">
+  </section>
+</html>
---
+++ database/seeds/examples/errors/503.blade.php
@@ -0,0 +1,15 @@
+<html>
+  <style>
+    body {
+        background: #333333 }
+    section {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        margin-right: -50%;
+        transform: translate(-50%, -50%) }
+  </style>
+  <section>
+    <img src="https://http.cat/503.jpg">
+  </section>
+</html>
